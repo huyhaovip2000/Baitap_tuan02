230b1219c85dd452e18ca1a66814f4e2ff3e51b3
Hao <tretrau2112000@gmail.com>
2021-03-22 23:12:50 +0700

    bai tap tuan 02

---
+++ app/src/main/java/com/example/baitap_tuan02/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.baitap_tuan02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+private TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+//        setContentView(R.layout.layout03);
+//        TextView tv=(TextView) findViewById(R.id.textView35);
+//        tv.setPaintFlags(tv.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+        // setContentView(R.layout.layout04);
+         setContentView(R.layout.layout01);
+        //setContentView(R.layout.layout02);
+    }
+    }
